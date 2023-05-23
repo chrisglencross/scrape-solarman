162d52a9c3ad5b72764cdaf3a3a1215019c32ff8
Chris Glencross <chris@raspberrypi4>
2023-05-23 10:50:40 +0100

    Update requests

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-for script in solarman-scraper zappi-scraper kia-scraper weather-scraper; do
+for script in solarman-scraper zappi-scraper kia-scraper weather-scraper octopus-scraper; do
   echo "Starting ${script}"
   ./ka.sh python ./${script}.py > ./logs/${script}.log 2>&1 &
 done
